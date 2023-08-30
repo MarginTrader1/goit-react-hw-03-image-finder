191382cb5ab63692b3878c4e508be511287ac462
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-08-30 11:46:25 +0300

    Update API.js

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const key = '37349612-6a9d2bc1c1c870fef97dab380';
 
 export const fetchImages = async (searchQuery, page) => {
-  const url = `https://pixabay.com/api/?key=${key}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+  const url = `https://pixabay.com/api/?key=${key}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=${page}`;
   const response = await axios.get(url);
   return response.data.hits;
 };
