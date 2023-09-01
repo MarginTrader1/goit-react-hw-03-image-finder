df167d6a1ca4579d82428e7cb61bac29501a3a7c
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-09-01 15:48:04 +0300

    сделал модалку

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,7 +1,54 @@
-export const ImageGalleryItem = ({ images }) => {
-  return images.map(image => (
-    <li key={image.id}>
-      <img src={image.webformatURL} alt={image.user} />
-    </li>
-  ));
+import { Component } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
 };
+
+Modal.setAppElement('#root');
+
+export class ImageGalleryItem extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  openModal = () => {
+    this.setState({ isModalOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ isModalOpen: false });
+  };
+
+  render() {
+    const { images } = this.props;
+
+    console.log(images);
+
+    return images.map(image => (
+      <li key={image.id}>
+        <img
+          src={image.webformatURL}
+          alt={image.user}
+          onClick={this.openModal}
+        />
+        <Modal
+          isOpen={this.state.isModalOpen}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <img src={image.largeImageURL} alt="large photo" />
+          <button onClick={this.closeModal}>close</button>
+        </Modal>
+      </li>
+    ));
+  }
+}
